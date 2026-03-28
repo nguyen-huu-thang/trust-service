ad0701f08da590e356ccb2e3835dbf95ae675f87
nguyễn thắng <nguyenhuuthang011@gmail.com>
2026-03-28 10:53:03 +0700

    first commit

---
+++ src/test/java/vn/xime/key/KeyApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.xime.key;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
